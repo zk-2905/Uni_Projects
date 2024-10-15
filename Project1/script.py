e3da784218623452181a7ab0fc14373cfbc69da5
Zaidh <zadykhan29@gmail.com>
2024-10-15 15:17:07 +0100

    restructured folder and added spelling checks

--- Project1/script.py
+++ Project1/script.py
@@ -10,13 +10,21 @@ def create_basedir():
         os.mkdir(path)
         print(f"{basedir} is created!")
 
-def create_new_workflow_stages():
+def create_new_workflow():
     workflow = str(input("Input Workflow name: "))
-    stage = str(input("Input Stage name:  "))
-    path1 = os.path.join(workflow)
-    path2 = os.path.join(stage)
-    os.mkdir(path1)
-    os.mkdir(path2)
+    if os.path.exists(workflow):
+        print("This Workflow already exists!")
+    else:
+        path = os.path.join(workflow)
+        os.mkdir(path)
+
+def create_new_stage():
+    stage = str(input("Input Stage name: "))
+    if os.path.exists(stage):
+        print("This Stage already exists!")
+    else:
+        path = os.path.join(stage)
+        os.mkdir(path)
 
 def create_meeting():
     filename = str(input("Enter filename: "))
@@ -35,18 +43,52 @@ def update_meeting():
 
 def main():
     true = True
+    more = True
     choice = str(input("Do you have an existing Base Directory: (y/n)"))
     if choice == 'n':
         create_basedir()
     basedir = str(input("Please enter your Base Directory name: "))
     os.chdir(basedir)
     print(f"You are in {basedir}")
-    workflow_stages = str(input("Do you want to create a new set of Workflow and Stages directory: "))
-    if workflow_stages == 'y':
-        create_new_workflow_stages()
+    workflow = str(input("Do you want to create a new Workflow directory: "))
+    if workflow == 'y':
+        create_new_workflow()
+        while more:
+            more_workflow = str(input("Do you want to create more Workflow directories (y/n): "))
+            if more_workflow == 'y':
+                create_new_workflow
+            else:
+                more = False
     while true:
-        folder = str(input("Do you want to enter into Workflow or Stage: "))
-        os.chdir(folder)
+        more = True
+        valid = True
+        while valid:
+            workflow = str(input("Enter Workflow name: "))
+            if os.path.exists(workflow):
+                os.chdir(workflow)
+                valid = False
+            else:
+                print("This workflow does not exists. Please try again!")
+        stage = str(input("Do you want to create a Stage directory: "))
+        if stage == 'y':
+            create_new_stage()
+            while more:
+                more_stage = str(input("Do you want to create more Stage directories (y/n): "))
+                if more_stage == 'y':
+                    create_new_stage()
+                else:
+                    more = False
+
+        valid = True
+        while valid:
+            stage = str(input("Enter Stage name: "))
+            if os.path.exists(stage):
+                os.chdir(stage)
+                valid = False
+            else:
+                print("This stage name does not exists. Please try again!")
+            
+
         option = str(input("To create a text file enter T/ To update a text file enter U: "))
         if option == 'T':
             create_meeting()
@@ -55,6 +97,5 @@ def main():
 
         home = os.path.expanduser(f"~/Uni//Uni_Projects/Project1/{basedir}") # this will be different on jupyterhub since your on local laptop rn
         os.chdir(home)
-        pass
 
 main()
